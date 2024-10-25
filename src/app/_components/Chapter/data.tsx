5fa71bf0cf9a7322c05e2d43238db6eecf3e7fc1
Juan Cortez <juan_cortez@utexas.edu>
2024-10-24 20:08:08 -0500

    add starter question

--- src/app/_components/Chapter/data.tsx
+++ src/app/_components/Chapter/data.tsx
@@ -15,10 +15,11 @@ ChapterData.set('1', {
   element: (
     <>
       <div>
+        <Question question="What are your initial thoughts after reading this chapter, did anything stand out to you?" />
         <Question question="How do we maintain our Christian identity in a world that may challenge our beliefs?" />
-        <Question question="What are modern-day examples of situations where we might need to draw a boundary, like Daniel and his friends did with food?" />
       </div>
       <div>
+        <Question question="What are modern-day examples of situations where we might need to draw a boundary, like Daniel and his friends did with food?" />
         <Question question="How can we find the balance between participating in culture and remaining faithful to God’s commands?" />
       </div>
     </>
