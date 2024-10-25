29aa7ad19fa47c1b993a57d3002d07b8004ea623
Juan Cortez <juan_cortez@utexas.edu>
2024-10-24 20:04:36 -0500

    add week 1

--- src/app/_components/Chapter/data.tsx
+++ src/app/_components/Chapter/data.tsx
@@ -10,15 +10,16 @@ export interface IChapterData {
 
 export const ChapterData = new Map<string, IChapterData>();
 ChapterData.set('1', {
-  book: 'Daniel - Week 1',
+  book: 'Faith in Exile',
   identifier: 'daniel-chapter-one',
   element: (
     <>
       <div>
-        <Question question="Placeholder question..." />
+        <Question question="How do we maintain our Christian identity in a world that may challenge our beliefs?" />
+        <Question question="What are modern-day examples of situations where we might need to draw a boundary, like Daniel and his friends did with food?" />
       </div>
       <div>
-        <Question question="Placeholder question..." />
+        <Question question="How can we find the balance between participating in culture and remaining faithful to God’s commands?" />
       </div>
     </>
   ),
