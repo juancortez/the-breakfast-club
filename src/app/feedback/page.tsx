eb3852ddd18562a9713e89175f9b33c8a93bc3ff
Juan Cortez <juan_cortez@utexas.edu>
2024-12-17 17:07:14 -0600

    feedback form

---
+++ src/app/feedback/page.tsx
@@ -0,0 +1,18 @@
+export default function FeedbackPage() {
+  return (
+    <section>
+      <h2 className="flex mb-4 text-2xl font-medium text-gray-900 dark:text-white">Breakfast Club Feedback</h2>
+      <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
+        Going into 2025, we are seeking feedback on what you&apos;d like to see more of!
+      </p>
+
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSd7B6hVoyTTxe1Z8vb7-s7dQc4ASy6ngmJ8N5VFAKGSmp4xcg/viewform?embedded=true"
+        width="640"
+        className="h-dvh"
+      >
+        Loading...
+      </iframe>
+    </section>
+  );
+}
