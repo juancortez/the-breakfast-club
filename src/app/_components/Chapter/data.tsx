adf40a3ef2e0ce048f2a1e0ed571ded31f7fe197
Juan Cortez <juan_cortez@utexas.edu>
2025-01-20 09:48:11 -0600

    add lesson

--- src/app/_components/Chapter/data.tsx
+++ src/app/_components/Chapter/data.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { Question } from '../Question';
 import { VerseTooltip } from '../VerseTooltip';
 
@@ -11,6 +12,17 @@ export interface IChapterData {
   gradient?: string;
 }
 export type ChapterTopic = 'the-book-of-daniel' | 'christmas-messy-or-amazing' | 'january-2025-sermons';
+
+const AppLink = ({ href, content }: { href: string; content: string }) => {
+  return (
+    <Link
+      href={href}
+      className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600 hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-300"
+    >
+      {content}
+    </Link>
+  );
+};
 
 const DanielChapterData = new Map<string, IChapterData>();
 DanielChapterData.set('1', {
@@ -198,6 +210,47 @@ Jan2025SermonsData.set('1', {
         <Question question="What areas of your life need the most alignment with your identity as a new creation?" />
         <Question question="What steps can you take this week to live more intentionally as a reflection of Christ?" />
         <Question question="How can spiritual disciplines such as prayer, fasting, and worship help you align with your identity as a new creation?" />
+      </div>
+    </>
+  ),
+}).set('2', {
+  book: 'Discussion',
+  title: 'Get Ready to Change the World',
+  identifier: 'questions',
+  overview: (
+    <p>
+      Discussion questions for <AppLink href="https://www.youtube.com/watch?v=MSMQRyOyLQw&t=1s" content="Get Ready to Change the World | Ethan Matott" />{' '}
+      sermon.
+    </p>
+  ),
+  element: (
+    <>
+      <div>
+        <Question question="How have you actively lived in the one area you felt God calling you to live more intentionally in the past week?" />
+        <Question question="What was the most impactful lesson this series has taught you so far?" />
+        <Question
+          question="Read Matthew 28:18-20. How can you shift from the mindset of unqualified to the mindset of being called according to His purpose?"
+          renderReference={() => (
+            <>
+              <VerseTooltip verse="Matthew 28:18-20" identifier="MAT.28.18-20" />
+            </>
+          )}
+        />
+        <Question question="What is one area of discipleship making you hesitant in that committed identity?" />
+        <Question question="How can daily sanctification shape and mold you into making a daily impact on the world?" />
+      </div>
+      <div>
+        <Question
+          question="Read John 13:34-35. How can knowing you have all that you need to be equipped move you to a place of confidence to go into the world?"
+          renderReference={() => (
+            <>
+              <VerseTooltip verse="John 13:34-35" identifier="jhn.13.34-35" />
+            </>
+          )}
+        />
+        <Question question="How does maintaining a heavenly perspective ease the anxiety of discipleship?" />
+        <Question question="What one thing do you need to let go of that is hindering you from paying the price for His mission?" />
+        <Question question="In what ways can you display and share the love of Christ through your words and deeds?" />
       </div>
     </>
   ),
