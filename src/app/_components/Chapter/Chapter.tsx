ea21f11468f568f6d1743a739a41b07ed6199a46
Juan Cortez <juan_cortez@utexas.edu>
2025-03-22 08:11:44 -0500

    spacing

--- src/app/_components/Chapter/Chapter.tsx
+++ src/app/_components/Chapter/Chapter.tsx
@@ -9,7 +9,7 @@ interface ChapterProps extends IChapterData {
 
 export function Chapter({ book, topic, overview, gradient = 'bg-gradient-to-r to-emerald-600 from-sky-400', children }: ChapterProps) {
   return (
-    <article>
+    <article className="mt-2">
       <h2 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-4xl">
         <span className={`text-transparent bg-clip-text ${gradient}`}>
           {topic} - <span>{book}</span>
